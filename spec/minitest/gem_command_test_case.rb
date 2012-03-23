36246572c626140dba000c7a0fcd3da68b10dd15
Joshua Priddle <jpriddle@me.com>
2012-03-23 03:20:06 -0400

    docs & cleanup

--- spec/minitest/gem_command_test_case.rb
+++ spec/minitest/gem_command_test_case.rb
@@ -5,42 +5,40 @@ require "tmpdir"
 require "fileutils"
 
 class Gem::Create::TestCase < MiniTest::Spec
-  before do
-    self.class.rm_tmpdir
-  end
+  # Apply this TestCase to any specs with
+  # `describe Gem::Commands::CreateCommand`
+  register_spec_type /Gem::Commands::CreateCommand/, Gem::Create::TestCase
 
+  # Clear the tmpdir if needed.
+  before { self.class.rm_tmpdir }
+
+  # Create a temporary directory.
   def self.tmpdir
     @tmpdir ||= Dir.mktmpdir
   end
 
+  # Remove the temporary directory.
   def self.rm_tmpdir
     FileUtils.rm_r(tmpdir) if File.exists?(tmpdir)
   ensure
     @tmpdir = nil
   end
 
-  def ui
-    @ui ||= Gem::MockGemUi.new
-  end
-
-  def use_ui(&block)
-    Dir.chdir(self.class.tmpdir) do
-      Gem::DefaultUserInteraction.use_ui(ui, &block)
-    end
-  end
-
-  def run_command(*args)
-    cmd = Gem::Commands::CreateCommand.new
-
-    capture_io { use_ui { cmd.invoke *args } }
-
-    return ui.output, ui.error
-  end
-
-  def file(path)
-    File.read(path)
-  end
-
+  # Shortcut for specs dealing with generated files.
+  #
+  # Asserts that the file exists within the temporary directory.
+  #
+  # path - Path to the subject file
+  #
+  # If a block is given, it yields the contents of the file.
+  #
+  # Examples:
+  #
+  #     it_renders "README" do |data|
+  #       data.must_match /Hello World!/
+  #     end
+  #
+  #     it_renders "Gemspec"
   def self.it_renders(path)
     it "renders #{path}" do
       file = File.join(self.class.tmpdir, path)
@@ -49,7 +47,27 @@ class Gem::Create::TestCase < MiniTest::Spec
       yield File.read(file) if block_given?
     end
   end
+
+  # Returns a Gem::MockGemUI. This allows us to work with IO in tests.
+  def ui
+    @ui ||= Gem::MockGemUi.new
+  end
+
+  # Run the CreateCommand with the specified arguments.
+  #
+  # Returns an Array, [0] is the output of the command, [1] is the error
+  # output.
+  def run_command(*args)
+    cmd = Gem::Commands::CreateCommand.new
+
+    Dir.chdir(self.class.tmpdir) do
+      Gem::DefaultUserInteraction.use_ui(ui) do
+        capture_io { cmd.invoke *args }
+      end
+    end
+
+    return ui.output, ui.error
+  end
 end
 
-MiniTest::Spec.register_spec_type /Gem::Commands/, Gem::Create::TestCase
 MiniTest::Unit.after_tests { Gem::Create::TestCase.rm_tmpdir }
