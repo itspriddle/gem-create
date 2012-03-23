5250eb844fec8c82491fcd5e812ea2f18ebbdd51
Joshua Priddle <jpriddle@me.com>
2012-03-23 02:49:48 -0400

    docs

--- lib/rubygems/commands/create_command.rb
+++ lib/rubygems/commands/create_command.rb
@@ -2,24 +2,31 @@ require 'rubygems/create/builder'
 require 'erb'
 require 'fileutils'
 
+# The Gem::Commands::CreateCommand class facilitates the `gem create` command.
 class Gem::Commands::CreateCommand < Gem::Command
+
+  # Default path to templates provided by this plugin.
   TEMPLATES = File.expand_path('../../create/templates', __FILE__)
 
+  # Initializes the plugin
   def initialize
     super "create", "Creates a new RubyGem skeleton"
     add_options!
   end
 
+  # Creates the gem.
   def execute
     create_gem!
   end
 
+  # The usage banner displayed for `gem create --help`
   def usage
     "gem create GEM_NAME"
   end
 
   private
 
+  # Private: Adds command line switches used by this plugin.
   def add_options!
     add_option "--force", "Overwrite existing files" do |force, options|
       options[:force] = !! force
@@ -50,6 +57,13 @@ class Gem::Commands::CreateCommand < Gem::Command
     end
   end
 
+  # Private: Creates the gem skeleton.
+  #
+  # First any requires directories are created. Then, templates are rendered
+  # via ERB and written as new files.
+  #
+  # When creating directories and files, any occurrence of 'gem_name' is
+  # replaced with the name of the gem to be created.
   def create_gem!
     manifest[:directories].each do |dir|
       FileUtils.mkdir_p dir.gsub('gem_name', builder.name)
@@ -61,16 +75,35 @@ class Gem::Commands::CreateCommand < Gem::Command
     end
   end
 
+  # Private: Returns true if files should be overwritten when creating the new
+  # gem.
+  #
+  # Returns true or false.
   def force?
     options[:force]
   end
 
+  # Private: The directory that contains template files to use when creating
+  # the new gem. By default, this is TEMPLATES. A user can specify their own
+  # directory with the `--template-directory` command line option.
+  #
+  # If a block is supplied, it is called within the template directory (i.e.
+  # Dir.pwd in the block will be set to the template directory), and the value
+  # of this block is returned.
+  #
+  # If no block is supplied, the String template directory is returned.
   def template_dir
     dir = options[:template_directory] || TEMPLATES
     Dir.chdir(dir) { return yield } if block_given?
     dir
   end
 
+  # Private: A Hash containing :files and :directories to create.
+  #
+  # :files       - An Array of files that will be created.
+  # :directories - An Array of directories that will be created.
+  #
+  # Returns a Hash.
   def manifest
     @manifest ||= Hash.new.tap do |h|
       template_dir do
@@ -81,6 +114,10 @@ class Gem::Commands::CreateCommand < Gem::Command
     end
   end
 
+  # Private: Writes a new file from a template.
+  #
+  # source - Path to the source template.
+  # dest   - Path to the destination file.
   def write_template_file(source, dest)
     if can_write_file?(dest)
       s = render_file(source)
@@ -94,10 +131,24 @@ class Gem::Commands::CreateCommand < Gem::Command
     force? || ! File.exists?(file)
   end
 
+  # Private: Renders a file via ERB within the context of a
+  # Gem::Create::Builder instance. This makes methods in that class available
+  # in the template.
+  #
+  # file - Path to the template file.
+  #
+  # Returns the rendered template as a String.
   def render_file(file)
     ERB.new(read_file(file)).result(builder.get_binding)
   end
 
+  # Private: Gets the content from the specified file.
+  #
+  # file - Path to the file to read.
+  #
+  # Raises RuntimeError if the file doesn't exist.
+  #
+  # Returns a String.
   def read_file(file)
     template_dir do
       if File.exists?(file)
@@ -108,6 +159,10 @@ class Gem::Commands::CreateCommand < Gem::Command
     end
   end
 
+  # Private: Creates/returns a Gem::Create::Builder instance. This will be
+  # used to provide variables in template files.
+  #
+  # Returns a Gem::Create::Builder instance.
   def builder
     @builder ||= Gem::Create::Builder.new(get_one_gem_name, options)
   end
