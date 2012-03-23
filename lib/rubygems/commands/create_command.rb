76d430cfbae772d96a993ee62a5c902509908d48
Joshua Priddle <jpriddle@me.com>
2012-03-23 01:16:40 -0400

    refactor builder

--- lib/rubygems/commands/create_command.rb
+++ lib/rubygems/commands/create_command.rb
@@ -1,6 +1,9 @@
 require 'rubygems/create/builder'
+require 'erb'
+require 'fileutils'
 
 class Gem::Commands::CreateCommand < Gem::Command
+  TEMPLATES = File.expand_path('../../create/templates', __FILE__)
 
   def initialize
     super "create", "Create a new gem"
@@ -8,8 +11,7 @@ class Gem::Commands::CreateCommand < Gem::Command
   end
 
   def execute
-    builder = Gem::Create::Builder.new(get_one_gem_name, self)
-    builder.render!
+    render!
   end
 
   private
@@ -30,5 +32,53 @@ class Gem::Commands::CreateCommand < Gem::Command
     add_option "--email EMAIL", "The author's email address used in gemspec" do |email, options|
       options[:email] = email
     end
+  end
+
+  def render!
+    manifest[:directories].each do |dir|
+      FileUtils.mkdir_p dir.gsub('gem_name', name)
+    end
+
+    manifest[:files].each do |file|
+      dest = file.gsub('gem_name', name).gsub(/\.erb$/, '')
+      write_template_file(file, dest)
+    end
+  end
+
+  def manifest
+    @manifest ||= Hash.new.tap do |h|
+      Dir.chdir TEMPLATES do
+        templates       = Dir["**/*"]
+        h[:files]       = templates.reject { |t| File.directory?(t) }
+        h[:directories] = templates.select { |t| File.directory?(t) }
+      end
+    end
+  end
+
+  def write_template_file(source, dest)
+    s = render_file(source)
+    File.open(dest, "w") { |file| file.puts s }
+  end
+
+  def render_file(file)
+    ERB.new(read_file(file)).result(builder.get_binding)
+  end
+
+  def read_file(file)
+    Dir.chdir TEMPLATES do
+      if File.exists?(file)
+        File.read(file)
+      else
+        raise "Template #{file.inspect} doesnt exist!"
+      end
+    end
+  end
+
+  def builder
+    @builder ||= Gem::Create::Builder.new(get_one_gem_name, options)
+  end
+
+  def name
+    builder.name
   end
 end
--- lib/rubygems/create/builder.rb
+++ lib/rubygems/create/builder.rb
@@ -1,57 +1,14 @@
-require 'erb'
-require 'fileutils'
-
 module Gem::Create
   class Builder
-    TEMPLATES = File.expand_path('../templates', __FILE__)
-
     attr_reader :name, :options
 
-    def initialize(name, parent_klass)
-      @options = parent_klass.options
+    def initialize(name, options = {})
+      @options = options
       @name    = name
     end
 
-    def render!
-      manifest[:directories].each do |dir|
-        FileUtils.mkdir_p dir.gsub('gem_name', name)
-      end
-
-      manifest[:files].each do |file|
-        dest = file.gsub('gem_name', name).gsub(/\.erb$/, '')
-        write_template_file(file, dest)
-      end
-    end
-
-    private
-
-    def manifest
-      @manifest ||= Hash.new.tap do |h|
-        Dir.chdir TEMPLATES do
-          templates       = Dir["**/*"]
-          h[:files]       = templates.reject { |t| File.directory?(t) }
-          h[:directories] = templates.select { |t| File.directory?(t) }
-        end
-      end
-    end
-
-    def write_template_file(source, dest)
-      s = render_file(source)
-      File.open(dest, "w") { |file| file.puts s }
-    end
-
-    def render_file(file)
-      ERB.new(read_file(file)).result(binding)
-    end
-
-    def read_file(file)
-      Dir.chdir TEMPLATES do
-        if File.exists?(file)
-          File.read(file)
-        else
-          raise "Template #{file.inspect} doesnt exist!"
-        end
-      end
+    def get_binding
+      binding
     end
 
     def author
@@ -81,6 +38,5 @@ module Gem::Create
     def gem_class
       name.split('-').map { |seg| seg.gsub(/(?:^|_| )(.)/) { $1.upcase } }.join('::')
     end
-
   end
 end
